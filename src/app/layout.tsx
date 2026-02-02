c7d49b5588f7dcee6025bdd9c0d7e52d8bdab337
Damjan Tepic <damjan.tepic@icloud.com>
2026-02-02 13:00:21 +0100

    v0.1.2
    Analytic added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/app/components/Footer";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
           className='flex flex-col justify-center items-center m-8'
       >
         {children}
+        <Analytics/>
         <Footer/>
       </body>
     </html>
