63fee1ab36ae934fe8f62f23bec33b54baec4f38
Damjan Tepic <damjan.tepic@icloud.com>
2026-02-02 16:24:03 +0100

    v0.1.5.2
    SpeedInsights Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/app/components/Footer";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics/>
+        <SpeedInsights/>
         <Footer/>
       </body>
     </html>
