91ab9ada157c462adf7fd48c1ecdd9964713dcc5
Damjan Tepic <damjan.tepic@icloud.com>
2026-08-13 18:44:29 +0200

    v1.8.2 Google analytic added

---
+++ src/app/components/GoogleAnalytics.tsx
@@ -0,0 +1,8 @@
+import { GoogleAnalytics as NextGoogleAnalytics } from "@next/third-parties/google";
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+export default function GoogleAnalytics() {
+  if (!GA_ID) return null;
+  return <NextGoogleAnalytics gaId={GA_ID} />;
+}
